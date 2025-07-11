74a87ce2a5b51f867a086ba7a0932f4f4bfb7434
Umang Ghatbandhe <umangghatbandhe0@gmail.com>
2025-07-11 17:13:12 +0530

    updated1.3

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -2,15 +2,13 @@ import Transaction from '@/models/Transaction';
 import { connectToDB } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
-type Params = { params: { id: string } };
-
-export async function DELETE(_: Request, { params }: Params) {
+export async function DELETE(_: Request, { params }: { params: { id: string } }) {
   await connectToDB();
   await Transaction.findByIdAndDelete(params.id);
   return NextResponse.json({ success: true });
 }
 
-export async function PATCH(req: Request, { params }: Params) {
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
   await connectToDB();
   const data = await req.json();
   const updated = await Transaction.findByIdAndUpdate(params.id, data, { new: true });
